53792ee071eb1bfecc2f14587f89322cd1163410
Dejan Trajkovic <dtrajko@gmail.com>
2020-04-09 16:08:55 +0200

    Domaci rad informatika 8. april

---
+++ domaci_rad.py
@@ -0,0 +1,13 @@
+print("Zvog virusa COVID-19 vreme kretanja je ograniceno.")
+godine = int(input("Unesite koliko imate godina: "))
+if godine <= 65: 
+    cas = int(input("Unesite cas BEZ minuta: "))
+    if cas in range(5,17):
+        print("Mozete da se krecete.")
+    else:
+        print("Ne mozete da se krecete.")
+        
+else:
+    print("Ne mozete da se krecete zbog starosti. Ostanite kod kuce!")
+    
+    
