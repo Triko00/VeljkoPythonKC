cb87c144843177a89ab12d5793f3cf8188b4f254
Dejan Trajkovic <dtrajko@gmail.com>
2020-04-09 16:17:21 +0200

    Domaci rad logicki operatori

--- veljko_logicki_operatori.py
+++ veljko_logicki_operatori.py
@@ -1,3 +1,7 @@
+# Domaci rad Informatika - Logicki operatori
+# Autor: Veljko Trajkovic
+# Datum: 9. april 2020
+
 print("Zvog virusa COVID-19 vreme kretanja je ograniceno.")
 godine = int(input("Unesite koliko imate godina: "))
 if godine <= 65: 
@@ -9,5 +13,3 @@ if godine <= 65:
         
 else:
     print("Ne mozete da se krecete zbog starosti. Ostanite kod kuce!")
-    
-    
